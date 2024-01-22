cd3b2a349dabc3a5948afe9406797ca439ff6a72
Lalit Mohan Bhatt <lb181808@gmail.com>
2024-01-22 21:47:06 +0530

    Episode 1

---
+++ _3CreatingSibling.js
@@ -0,0 +1,26 @@
+/*How can we create below type of structure using react->
+
+<div id="parent">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+        <h1>I am anotherh1 tag</h1>
+        
+    </div>
+<div>
+
+*/
+
+
+//here parent is the object and when we are writing resut.render(parent) then parent object is being converted into an html element
+const parent=React.createElement(
+    "div",
+{id:"parent"},
+React.createElement(
+    "div",
+    {id:"child"}
+    ,[React.createElement("h1",{},"I am an h1 tag"),React.createElement("h1",{},"I am another h1 tag")]
+    )
+)
+
+const result=ReactDOM.createRoot(document.querySelector("body"));
+result.render(parent);
